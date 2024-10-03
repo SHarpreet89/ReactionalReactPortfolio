fd799992d7e5778981e2cd58c2b219da6b35bb09
Harpreet Singh <S.harpreet@me.com>
2024-10-03 23:40:48 +1000

    Webstite with prelim design

---
+++ src copy/components/About/index.jsx
@@ -0,0 +1,25 @@
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+          Hi, I'm Harpreet Singh
+        </p>
+        <p>
+          Welcome to my portfolio.
+          I am Harpreet Singh, a dedicated and passionate full-stack web developer with a 
+          background in Architecture and Design. My experience with some of the world's 
+          leading architecture firms has honed my ability to merge aesthetic principles with 
+          technical precision. I bring a keen eye for detail and a commitment to delivering visually 
+          appealing and highly functional web solutions. Explore my work and see how I can bring your 
+          digital projects to life.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
